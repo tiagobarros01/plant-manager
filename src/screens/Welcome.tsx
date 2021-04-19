4fd73ded0a40a37c4dc7d4859911227fb181b0a3
tiagobarros01 <tbarros1337@hotmail.com>
2021-04-19 15:16:14 -0300

    feat: finish Welcome screen

--- src/screens/Welcome.tsx
+++ src/screens/Welcome.tsx
@@ -1,28 +1,30 @@
 /* eslint-disable import/extensions */
 import React from 'react';
-import {
-  Text, Image, TouchableOpacity,
-} from 'react-native';
 
 import wateringImg from '../assets/watering.png';
-import { Title, Wrapper } from '../styles/screens/Welcome';
+import { Button } from '../components/TouchableButton';
+import {
+  SubTitle, Title, Wrapper, Image,
+} from '../styles/screens/Welcome';
 
 export function Welcome() {
   return (
     <Wrapper>
-      <Title>Gerencie suas plantas de forma fácil</Title>
+      <Title>
+        Gerencie
+        {'\n'}
+        suas plantas
+        {'\n'}
+        de forma fácil
+
+      </Title>
 
       <Image source={wateringImg} />
-      <Text>
+      <SubTitle>
         Não esqueça mais de regar suas plantas.
         Nós cuidamos de lembrar você sempre que precisar.
-      </Text>
-
-      <TouchableOpacity>
-        <Text>
-          {'>'}
-        </Text>
-      </TouchableOpacity>
+      </SubTitle>
+      <Button title=">" />
     </Wrapper>
   );
 }
--- src/styles/screens/Welcome.ts
+++ src/styles/screens/Welcome.ts
@@ -3,20 +3,26 @@ import styled from 'styled-components/native';
 export const Wrapper = styled.SafeAreaView`
   flex: 1;
   align-items: center;
-  justify-content: center;
+  justify-content: space-between;
 `;
 
 export const Title = styled.Text`
   font-size: 32;
   font-weight: bold;
   text-align: center;
-  color: ${(props) => props.theme.colors.blue};
+  color: ${(props) => props.theme.colors.heading};
+  margin-top: 38;
 `;
+
+export const Image = styled.Image`
+    width: 292;
+    height: 284;
+  `;
 
 export const SubTitle = styled.Text`
-  
-`;
-
-export const Button = styled.Button`
-  
+  text-align: center;
+  font-size: 18;
+  padding-left: 20;
+  padding-right: 20;
+  color: ${(props) => props.theme.colors.heading};
 `;

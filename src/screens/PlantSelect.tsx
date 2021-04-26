2b97d80bf5a3b8f3e4f8cf16df97fc9719e99525
tiagobarros01 <tbarros1337@hotmail.com>
2021-04-26 13:11:33 -0300

    chore: installed axios & created a api

--- src/screens/PlantSelect.tsx
+++ src/screens/PlantSelect.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-use-before-define */
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { FlatList } from 'react-native-gesture-handler';
